ccadb1f8f3edac75ab9a7114d863fa3fb94e8aa0
WxWyashen <21474008@qq.com>
2019-03-11 15:45:49 +0800

    First Commit. Adding a README.

---
+++ Real.v
@@ -0,0 +1,377 @@
+(** * Real: Simple real number definition *)
+
+Set Warnings "-notation-overridden,-parsing".
+From Coq Require Import Bool.Bool.
+From Coq Require Import Init.Nat.
+From Coq Require Import Arith.Arith.
+From Coq Require Import Arith.EqNat.
+From Coq Require Import omega.Omega.
+From Coq Require Import Lists.List.
+From Coq Require Import Strings.String.
+From Coq Require Import QArith.QArith_base.
+Import ListNotations.
+
+Module Definition_1.
+(** In this part, we use Dedekind Cut to constructing real numbers.*)
+
+(** First, we shoule know what is Dedekind Cut.
+    According to Rudin's "principles_of_mathematical_analysis",
+    a Cut is any set A which is a subset of Q with the following three properties,
+      i. A is not empty, and A != Q
+      ii. If p is in A, q is in Q, and q < p  =>  q is in A
+      iii. If p is in A, => exists r in A, p < r.
+    And we call the cuts as the members of R.
+    We use the letters p,q,r to denote rational numbers and A,B,C to denote the cuts.
+*)
+
+Record Dedekind ( A : Q-> Prop) : Prop := {
+  Dedekind_properties1 : (exists (x : Q) , A x) /\ (exists (x : Q) , ~ A x) ;
+  Dedekind_properties2 : forall (p q : Q) , A p /\ (Qle q p) -> A q ;
+  Dedekind_properties3 : forall (p : Q) , A p -> (exists r, A r /\ (Qlt p r)) ;
+  Dedekind_properties4 : forall (p q : Q), p == q -> A p -> A q ;
+}.
+
+Inductive Real : Type :=
+  | Real_cons (A : Q -> Prop) (H : Dedekind A)
+.
+
+(** Then we will add some properties to ensure R become a field.*)
+
+(** First , we find some members to denote zero and one.*)
+
+Theorem Dedekind_Q : forall x1 : Q , Dedekind (fun x=> x < x1).
+Proof.
+  split.
+  - split.
+    Search Qlt.
+    exists (x1 - 1).
+    + intros. unfold Qlt.
+      simpl.
+      rewrite -> Zmult_plus_distr_l.
+      replace (Z.pos (Qden x1 * 1) % positive) with (QDen x1 * 1)%Z by reflexivity.
+      repeat rewrite -> Zmult_1_r.
+      rewrite <- Z.add_0_r.
+      apply Zplus_lt_compat_l.
+      reflexivity.
+    + exists x1. apply Qlt_irrefl.
+  - intros.
+    inversion H.
+    apply Qle_lt_trans with (y := q).
+    apply Qle_lteq. right. reflexivity.
+    apply Qle_lt_trans with (y := p). apply H1.
+    apply H0.
+  - intros.
+    Search Qmake.
+    exists ((p + x1) * / (2 # 1) ).
+    split.
+    + apply Qlt_shift_div_r. 
+      reflexivity.
+      assert(H' : x1 * (2 # 1) == x1 + x1).
+      {
+        unfold Qmult.
+        unfold Qplus.
+        simpl. rewrite <- Zmult_plus_distr_l.
+        rewrite Zmult_comm.
+        rewrite <- Z.add_diag.
+        unfold Qeq.
+        simpl. rewrite -> Pos.mul_1_r.
+        rewrite Pos2Z.inj_mul.
+        rewrite Zmult_assoc. reflexivity.
+      }
+      rewrite H'.
+      apply Qplus_lt_le_compat. apply H. apply Qle_refl.
+    + apply Qlt_shift_div_l. 
+      reflexivity.
+      assert(H' : p * (2 # 1) == p + p).
+      {
+        unfold Qmult.
+        unfold Qplus.
+        simpl. rewrite <- Zmult_plus_distr_l.
+        rewrite Zmult_comm.
+        rewrite <- Z.add_diag.
+        unfold Qeq.
+        simpl. rewrite -> Pos.mul_1_r.
+        rewrite Pos2Z.inj_mul.
+        rewrite Zmult_assoc. reflexivity.
+      }
+      rewrite H'.
+      apply Qplus_lt_r. apply H.
+  - intros. 
+    apply Qle_lt_trans with p.
+    + apply Qle_lteq. right.
+      symmetry. apply H.
+    + apply H0. 
+Qed.
+
+Definition Rzero : Real := Real_cons (fun x => x < 0) (Dedekind_Q 0).
+
+Definition Rone : Real := Real_cons (fun x => x < 1) (Dedekind_Q 1).
+
+(** Next, we will define the plus operation and the mult operation. *)
+(** First, we will define order of Real and proof some theorem about the order.*)
+Definition Rle (a b:Real) : Prop :=
+  match a with
+    | Real_cons A HA => match b with
+                          | Real_cons B HB => (forall x , A x -> B x)
+                        end
+  end.
+
+Notation "a <= b" := (Rle a b).
+
+Definition Rlt (a b:Real) : Prop :=
+  match a with
+    | Real_cons A HA => match b with
+                          | Real_cons B HB => (forall x , A x -> B x) /\ (exists x , B x /\ ~(A x))
+                        end
+  end.
+
+Notation "a < b" := (Rlt a b).
+
+Definition Reqb(a b : Real) : Prop := a <= b /\ b <= a.
+
+Notation "a == b" := (Reqb a b).
+
+Theorem Rle_refl: forall x : Real, x <= x.
+Proof.
+  intros.
+  destruct x.
+  unfold Rle.
+  intros. apply H0.
+Qed.
+
+Theorem Rlt_le_weak: forall x y : Real, x < y -> x <= y.
+Proof.
+  intros.
+  destruct x.
+  destruct y.
+  inversion H.
+  unfold Rle.
+  apply H2.
+Qed.
+
+Theorem Rle_trans: forall x y z : Real, x <= y -> y <= z -> x <= z.
+Proof.
+  intros.
+  destruct x.
+  destruct y.
+  destruct z.
+  unfold Rle in *.
+  intros.
+  apply H0. apply H. apply H4.
+Qed.
+
+Theorem Rlt_trans: forall x y z : Real, x < y -> y < z -> x < z.
+Proof.
+  intros.
+  destruct x.
+  destruct y.
+  destruct z.
+  unfold Rlt in *.
+  split.
+  - intros. apply H0. apply H. apply H4.
+  - destruct H. destruct H0. inversion H4. inversion H5. 
+    exists x. split. apply H0. apply H6. apply H6.
+Qed.
+
+Theorem Rnot_le_lt: forall x y : Real, ~ x <= y -> y < x.
+Proof.
+Admitted.
+Theorem Rnot_lt_le: forall x y : Real, ~ x < y -> y <= x.
+Proof.
+  intros.
+  destruct x.
+  destruct y.
+  unfold not in *.
+  unfold Rlt in *.
+  unfold Rle.
+  
+Admitted.
+Theorem Rle_not_lt: forall x y : Real, x <= y -> ~ y < x.
+Proof.
+Admitted.
+Theorem Rlt_not_le: forall x y : Real, x < y -> ~ y <= x.
+Proof.
+Admitted.
+Theorem Rle_antisym: forall x y : Real, x <= y -> y <= x -> x == y.
+Proof.
+  intros.
+  unfold Reqb.
+  split. apply H. apply H0.
+Qed.
+Theorem Rle_lt_trans: forall x y z : Real, x <= y -> y < z -> x < z.
+Proof.
+Admitted.
+Theorem Rlt_le_trans: forall x y z : Real, x < y -> y <= z -> x < z.
+Proof.
+Admitted.
+
+(** Second , we will difine the plus operation of Set and Real and proof some theorem about it. *)
+
+Definition Cut_plus_Cut (A : Q -> Prop) (B : Q -> Prop) : Q -> Prop :=
+  (fun x => exists x0 x1 : Q, A x0 /\ B x1 /\ (Qeq (x0 + x1) x))
+.
+
+Lemma Dedekind_le : forall A x x1, Dedekind A -> (~ A x) -> A x1 -> x > x1.
+Proof.
+  intros.
+  destruct H.
+  apply Qnot_le_lt.
+  unfold not.
+  intros. 
+  apply H0. 
+  apply Dedekind_properties6 with x1.
+  split.
+  apply H1.
+  apply H.
+Qed.
+
+Theorem Dedekind_plus : forall A B, Dedekind A -> Dedekind B -> Dedekind (Cut_plus_Cut A B).
+Proof.
+  intros.
+  inversion H.
+  inversion H0.
+  split.
+  - destruct Dedekind_properties5.
+    destruct Dedekind_properties9.
+    split.
+    + inversion H1.
+      inversion H3.
+      exists (x0 + x), x , x0.
+      split.
+      * apply H5.
+      * split. 
+        apply H6.
+        apply Qplus_comm.
+    + inversion H2. inversion H4.
+      unfold not in *.
+      exists (x + x0).
+      intros. destruct H7. destruct H7.
+      assert (H' : x > x1 /\ x0 > x2).
+      {
+        split.
+        apply Dedekind_le with A.
+        apply H. unfold not. apply H5. apply H7.
+        apply Dedekind_le with B.
+        apply H0. unfold not. apply H6. apply H7.
+      }
+      assert (H'' : Qlt (x1 + x2)(x + x0)).
+      {
+        apply Qplus_lt_le_compat.  apply H'.
+        apply Qlt_le_weak.  apply H'.
+      }
+      destruct H7.  destruct H8. rewrite H9 in H''.
+      apply Qlt_irrefl with (x + x0). apply H''.
+  - intros.
+    destruct H1. destruct H1. destruct H1.
+    unfold Cut_plus_Cut.
+    exists x , (q + - x).
+    split. apply H1.
+    split. apply Dedekind_properties10 with x0.
+    split. apply H1. 
+    apply Qplus_le_l with (z := x).
+    destruct H1. destruct H3.
+    rewrite Qplus_comm in H4.
+    rewrite H4.  apply Qle_trans with q.
+    rewrite <- Qplus_assoc.  rewrite (Qplus_comm (- x)).
+    rewrite Qplus_opp_r.  rewrite Qplus_0_r.
+    apply Qle_refl. apply H2.
+    rewrite Qplus_comm. rewrite <- Qplus_assoc.
+    rewrite (Qplus_comm (- x)).  rewrite Qplus_opp_r.
+    apply Qplus_0_r.
+  - intros.
+    repeat destruct H1. destruct H2.
+    apply Dedekind_properties7 in H1.
+    apply Dedekind_properties11 in H2 as goal.
+    destruct H1. destruct goal.
+    exists (x1 + x0).
+    split. unfold Cut_plus_Cut.
+    exists x1,x0.
+    split. apply H1.
+    split. apply H2. reflexivity.
+    rewrite <- H3.
+    apply Qplus_lt_l.  apply H1.
+  - intros.
+    repeat destruct H2.
+    unfold Cut_plus_Cut.
+    exists x,x0.
+    split. apply H2.
+    split. apply H3.
+    rewrite <- H1. apply H3.
+Qed.
+
+Fixpoint Rplus(a b : Real) : Real :=
+  match a with
+    | (Real_cons A HA) => match b with
+                            | (Real_cons B HB) =>
+                               Real_cons (Cut_plus_Cut A B) (Dedekind_plus A B HA HB)
+                          end
+  end.
+
+Notation "A + B" := (Rplus A B).
+
+Theorem Rplus_O : forall a : Real, a + Rzero == a.
+Proof.
+Admitted.
+
+Theorem Rplus_comm : forall a b : Real, a + b == b + a.
+Proof.
+Admitted.
+
+Theorem Rplus_assoc : forall a b c : Real, a + b + c == a + (b + c).
+Proof.
+Admitted.
+
+Theorem Rplus_compat_l :
+  forall a b c : Real, a + b == a + c -> b == c.
+Proof.
+Admitted.
+
+Definition Cut_opp (A : Q -> Prop) : Q -> Prop :=
+  (fun x => exists r : Q, r > 0 -> ~(A (Qplus (-x) (-r))))
+.
+
+Theorem Dedekind_opp : forall A : Q -> Prop , Dedekind A -> Dedekind (Cut_opp A).
+Proof.
+Admitted.
+
+Definition Ropp (a : Real) : Real :=
+  match a with
+    | Real_cons A HA => Real_cons (Cut_opp A) (Dedekind_opp A HA)
+  end.
+
+Notation " - a" := (Ropp a).
+
+Theorem Rplus_opp :
+  forall a : Real, a + - a == Rzero.
+Proof.
+Admitted.
+
+Theorem Rmult_O : forall a : Real, a * Rzero == Rzero.
+Proof.
+Admitted.
+
+Theorem Rmult_1 : forall a : Real, a * Rone == a.
+Proof.
+Admitted.
+
+Theorem Rmult_comm : forall a b : Real, a * b == b * a.
+Proof.
+Admitted.
+
+Theorem Rmult_assoc : forall a b c : Real, a * b * c == a * (b * c).
+Proof.
+Admitted.
+
+Theorem Rmult_distr :
+  forall a b c : Real, a * (b + c) == (a * b) + (a * c).
+Proof.
+Admitted.
+
+Theorem Rmult_inv :
+  forall a : Real, (a <> Rzero) -> exists b, a * b == Rone.
+Proof.
+Admitted.
+
+End Definition_1.
+
+(* Sat 2019.3.2 16:37 Wu Xiwei , Hu Zhixuan *)
