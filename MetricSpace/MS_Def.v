21807be537dfb14172c47e4e6386065b5522722a
QinxiangCao <caoqinxiang@gmail.com>
2019-03-22 13:04:01 +0800

    Code review 0322.

--- MetricSpace/MS_Def.v
+++ MetricSpace/MS_Def.v
@@ -1,12 +1,14 @@
 From Coq Require Import Init.Nat.
 (**def of total-order-field and metric space**)
-Class OrderField (X : Type) :={
+Class OrderField (X : Type): Type :={
     lt : X -> X -> Prop;
     HOF1 : forall (x y : X), lt x y \/ lt y x;
     HOF2 : forall (x : X), ~(lt x x);
     HOF3 : forall (x y z : X), lt x y -> lt y z -> lt x z;
 }.
-
+Notation "x < y" := (lt x y): metric.
+Local Open Scope metric.
+(** Maybe using notation to make code more readable. -- Qinxiang *)
 Class Field  (X : Type)  :={
     x0 : X;
     plus : X -> X -> X;
@@ -16,8 +18,7 @@ Class Field  (X : Type)  :={
     HF4 : forall (x : X), (exists ix, plus x ix = x0) ;
 }.
 
-Class Metric (A : Type) :={
-    X : Type;
+Class Metric (A : Type) (X : Type):={
     dist : A -> A -> X;
     HM_F :> Field X;
     HM_OF :> OrderField X;
@@ -26,9 +27,10 @@ Class Metric (A : Type) :={
     HM3 : forall (p : A), dist p p = x0;
     HM4 : forall (p1 p2 p3 : A), (lt (dist p1 p3) (plus (dist p1 p2) (dist p2 p2))) \/ (dist p1 p3) = (plus (dist p1 p2) (dist p2 p2));
 }.
-
-Class CauchySeq (A : Type) :={
-    HC_M : Metric A;
+(** Putting Type definitions in to type class arguments
+are probably better. -- Qinxiang *)
+Class CauchySeq (A : Type) (X : Type) :={
+    HC_M : Metric A X;
     Cseq : nat -> A;
     HCA : forall (eps : X), lt x0 eps
       -> (exists (N : X -> nat), forall (m n:nat), (m > (N eps))%nat /\ (n > (N eps))%nat
