5d06ac5994aa031430ce4cbf578bdcdde5463a8e
shrub_broom <shrubbroom@sjtu.edu.cn>
2019-04-03 09:52:39 +0000

    MS_Def.v edited on 04/03 : a fatal fault occurs when defining Cauchy sequence in this version (and version before, line 56), update only for reserving and future reference.

--- MetricSpace/MS_Def.v
+++ MetricSpace/MS_Def.v
@@ -1,7 +1,14 @@
-From Coq Require Import Init.Nat.
-From Coq Require Import Classes.RelationClasses.
+From Coq Require Import Bool.Bool.
 From Coq Require Import Logic.Classical.
+From Coq Require Import Init.Nat.
+From Coq Require Import Arith.Arith.
+From Coq Require Import Arith.EqNat.
+From Coq Require Import omega.Omega.
+From Coq Require Import Lists.List.
+From Coq Require Import Strings.String.
+From Coq Require Import Classes.RelationClasses.
 (** def of preorder-field and metric space **)
+
 
 Class preOrderField (X : Type) :={
     le : X -> X -> Prop;
@@ -34,11 +41,9 @@ Class Metric (A : Type) (X : Type):={
 }.
 (** def of notations with clear meanings **)
 Notation "x <= y" := (le x y): metric.
-Local Open Scope metric.
-Notation "| x , y |" := (dist x y) : metric.
+Notation " ( x , y ) " := (dist x y) : metric.
 Local Open Scope metric.
 Notation "x + y" := (plus x y) : Field.
-Local Open Scope Field.
 (** Putting Type definitions in to type class arguments
 are probably better. -- Qinxiang *)
 Definition seq {A : Type} :=nat -> A -> Prop.
@@ -49,7 +54,7 @@ Class CauchySeq (A : Type) (X : Type) (M : Metric A X) (Cseq : seq):={
     HCseq2 : forall(m : nat) (a1 a2 : A), (a1 = a2) -> (Cseq m a1 <-> Cseq m a2);
     HCseq3 : forall(m : nat) (a1 a2 : A), Cseq m a1 -> Cseq m a2 -> (a1 = a2);
     HCA : forall (eps : X), le x0 eps
-      -> (exists (N : nat), forall (m n:nat), (m > N)%nat /\ (n > N)%nat
+      -> (exists (N : nat), forall (m n:nat), (N < m)%nat /\ (N < n)%nat
             -> forall (a b : A), Cseq m a /\ Cseq n b
          -> (dist a b) <= eps);
 }.
@@ -58,19 +63,19 @@ Class Converge (A : Type) (X : Type)  (M : Metric A X) (Seq : seq):={
     HC1 : forall(n : nat), (exists (a : A), Seq n a);
     HC2 : forall(m : nat) (a1 a2 : A), (a1 = a2) -> (Seq m a1 <-> Seq m a2);
     HC3 : forall(m : nat) (a1 a2 : A), Seq m a1 -> Seq m a2 -> (a1 = a2);
-    HC : forall (eps : X), (exists (N : nat) (lim : A) , forall (n : nat) (a : A), (n > N)%nat -> Seq n a
+    HC : forall (eps : X), (exists (N : nat) (lim : A) , forall (n : nat) (a : A), (N < n)%nat -> Seq n a
       -> (dist a lim) <= eps);
 }.
 
-Inductive Cauchilize (A : Type) (X : Type) : Type :=
-  | con_intro (Cseq : seq) (M : Metric A X) (H : CauchySeq A X M Cseq) .
+Inductive Cauchilize {A X : Type}  {M : Metric A X}  : Type :=
+  | con_intro (Cseq : seq)(H : CauchySeq A X M Cseq) .
 
 Definition refl {A : Type} (reflseq : seq) (a : A) : Prop :=
   (forall (n : nat), reflseq n a) /\ (forall (a' : A) (n : nat) , a <> a' -> ~(reflseq n a')).
 
 
 Theorem c_trans :
-    forall (A X : Type) (M : Metric A X) (reflseq : seq) (a : A),refl reflseq a
+    forall {A X : Type} (M : Metric A X) (reflseq : seq) (a : A),refl reflseq a
       -> CauchySeq A X M reflseq.
 Proof.
     intros. unfold refl in H. split. + intros. exists a. apply H.
@@ -86,50 +91,82 @@ Proof.
     apply H5. apply H2. }
     apply NNPP in H5.
     assert(~(a <> b)). unfold not in H4. unfold not. intros. apply H4 with (n := n) in H6.
-   apply H6. apply H3.     apply not_one_another_true in H6.
+   apply H6. apply H3.     apply NNPP in H6.
     rewrite <-H6, <-H5. assert(dist a a  = x0). apply HM3. rewrite H7.
     apply H0.
 Qed.
 
-Definition Ctr {A X  :Type} : A -> Cauchilize A X.
-Proof.
-  intros.
+Definition A_ := Type.
+Definition X_ := Type.
 
+Definition ctr {A X : Type}  (M : Metric A X)  (a : A) (reflseq : seq) (H : refl reflseq a) : Cauchilize :=
+    con_intro reflseq (c_trans M reflseq a H).
 
-
-Definition equC {A X : Type} (x1 : Cauchilize A X) (x2 : Cauchilize A X) :  Prop  :=
+Definition equC {A X : Type} {M : Metric A X} (x1 x2 : @Cauchilize A X M):  Prop  :=
   match x1,   x2 with
-    | con_intro _ _ cseq1 M1 H1, con_intro _ _ cseq2 M2 H2 =>
+    | con_intro cseq1 C1, con_intro cseq2 C2 =>
         (forall (eps : X), x0 <= eps
-             -> (exists (N : nat), forall (n :nat), (n > N)%nat
+             -> (exists (N : nat), forall (n :nat), (N < n)%nat
              -> forall (a1 a2 : A), cseq1 n a1  -> cseq2 n a2
              -> (dist a1 a2) <= eps))
   end.
 Notation "a == b" := (equC a b)
     (at level 70, no associativity) : equC.
-Definition leC {X : Type} (x1 : Cauchilize X X) (x2 : Cauchilize X X)  : Prop :=
+Definition leC {X : Type} {M : Metric X X} (x1 x2 : @Cauchilize X X M) : Prop :=
     match x1,   x2 with
-    | con_intro _ _ cseq1 _ _, con_intro _ _ cseq2 _ _ =>
-        (exists (N  : nat), forall (n : nat), (n > N)%nat
+    | con_intro cseq1 C1, con_intro cseq2 C2 =>
+        (exists (N: nat), forall (n : nat), (N < n)%nat
             ->  forall (a1 a2 : X), cseq1 n a1  -> cseq2 n a2
-             -> a1 <= a2) \/ equC x1 x2
+             -> a1 <= a2) \/ (equC x1 x2)
     end.
 
 Notation " x1 <= x2" := (leC x1 x2).
+(**
+Definition siCauchilize : Type :=(@Cauchilize A_ X_ ).
+Definition xsiCauchilize : Type :=(@Cauchilize X_ X_) .
+Definition biCauchilize: Type := @Cauchilize (@Cauchilize A_ X_) (@Cauchilize X_ X_). **)
 
-Definition A := Type .
-Definition X := Type .
-
-
-Definition siCauchilize : Type :=(Cauchilize A X).
-Definition xsiCauchilize : Type :=(Cauchilize X X) .
-Definition biCauchilize: Type := Cauchilize (Cauchilize A X) (Cauchilize X X).
-
-Theorem preOrder_trans : forall (X : Type), preOrderField X -> dense X ->
-   preOrderField (Cauchilize X X).
+Lemma refl_equC : forall {A X : Type} {M : Metric A X} (x : @Cauchilize A X M), equC x x.
 Proof.
-  intros. split with (le := leC). +intros. unfold leC.
-    inversion X2. 
+  intros. unfold equC. destruct x. intros. inversion H. apply HCA0 in H0.
+  destruct H0. exists x. intros. assert(x < n /\  x < n). split. apply H1. apply H1.
+  apply H0 with (a := a1) (b := a2) in H4 . apply H4. split. apply H2. apply H3.
+Qed.
+
+Lemma le_one : forall (m n : nat), (m <= n)%nat \/ (n <= m)%nat.
+Proof.
+  intro m. induction m.
+  -intros. left. induction n. apply le_n. apply le_S. apply IHn.
+  -intros. destruct n. +right. apply le_0_n. +destruct IHm with (n := n).
+  {left. apply le_n_S. auto. } {right. apply le_n_S. auto. }
+Qed.
+
+Lemma le_equ : forall (m n : nat), (m <= n)%nat -> (n <= m)%nat -> m = n.
+Proof.
+  intro m. induction m as [| m' IH]. -intros. destruct n. auto. inversion H0.
+  -intros. destruct n. +inversion H. +apply le_S_n in H. apply le_S_n in H0.
+    assert (m' = n). apply IH. apply H. apply H0. auto.
+Qed.
+
+Theorem preOrder_trans : forall {X : Type} {M : Metric X X}, preOrderField X ->
+   preOrderField (@Cauchilize X X M).
+Proof.
+  intros. split with (le := leC). -intros. unfold leC. destruct x. right. apply refl_equC.
+  -intros. unfold leC. destruct x. destruct y. destruct z. unfold leC in H, H0.
+    destruct H. destruct H0. +left. destruct H. destruct H0. assert( (x1 <= x)%nat\/ (x <= x1)%nat ).
+    apply le_one. destruct H4. exists x. intros. inversion H2. destruct HCseq4 with (n := n). apply H with (a2 := x2) in H6.
+    apply H0 with (a1 := x2) in H7. {apply (HOF3 a1 x2 a2). apply H6. apply H7. }
+    {assert(forall x y : nat, (x <= y)%nat -> x < y \/ x = y). intros. apply le_lt_or_eq. auto.
+    -apply H9 in H4. destruct H4. + apply (lt_trans x1 x n).
+    apply H4. apply H5. +rewrite H4. auto. }
+    apply H8. apply H5. apply H8.
+    exists x1. intros. inversion H2. destruct HCseq4 with (n := n). apply H with (a2 := x2) in H6.
+    apply H0 with (a1 := x2) in H7. {apply (HOF3 a1 x2 a2). apply H6. apply H7. }
+    apply H5. apply H8. assert(forall x y : nat, (x <= y)%nat -> x < y \/ x = y). intros. apply le_lt_or_eq. auto.
+    apply H9 in H4. destruct H4. apply (lt_trans x x1 n).
+    auto. auto. rewrite <-H4 in H5. auto. auto.
+
+(** warning : stop running !**)
 
 Theorem metric_trans : Metric A X -> Metric (Cauchilize A X) (Cauchilize X X) .
 Proof.
