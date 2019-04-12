ff961a2f7824b06283c3f237da122abe85fc1e11
zoulikai <linton@sjtu.edu.cn>
2019-04-12 10:56:48 +0800

    definition

--- Iso/Bijection.v
+++ Iso/Bijection.v
@@ -1,58 +1,25 @@
-(* Third Section: C2D *)
+Set Warnings "-notation-overridden,-parsing".
+From Coq Require Import Bool.Bool.
+From Coq Require Import Logic.Classical.
+From Coq Require Import Init.Nat.
+From Coq Require Import Arith.Arith.
+From Coq Require Import Arith.EqNat.
+From Coq Require Import omega.Omega.
+From Coq Require Import Lists.List.
+From Coq Require Import Strings.String.
+From Coq Require Import QArith.QArith_base.
+From Coq Require Import QArith.Qabs.
+From Coq Require Import QArith.Qminmax.
+From Coq Require Import Logic.Classical.
+From Coq Require Import Classes.Equivalence.
+From Coq Require Import Classes.Morphisms.
+From CReal Require Import Dedekind.RBase.
+From CReal Require Import Dedekind.ROrder.
+From CReal Require Import Dedekind.RArith.
+Require Import CReal.Cauchy.RBase_uncomp.
+From Coq Require Import PArith.BinPosDef.
+Import Pos.
 
-Theorem Dedekind_CSeq :forall (CSeq:nat->Q->Prop),
-Cauchy CSeq->Dedekind (fun q=>exists (N:nat),forall (n:nat),(n>N)%nat->(forall(p:Q),CSeq n p->(q<p)%Q)).
-Proof.
-Admitted.
-
-Definition C2D (A:CReal):DReal :=
-match A with
-|Real_intro CSeq H =>
-Real_cons (fun q=>exists (N:nat),forall (n:nat),(n>N)%nat->(forall(p:Q),CSeq n p->(q<p)%Q)) (Dedekind_CSeq CSeq H) end.
-
-Theorem C2D_properity1:forall (x y:CReal),
-  (((C2D x)+(C2D y))%DR==C2D (x+y)%CR)%DR.
-Proof.
-Admitted.
-
-Theorem C2D_properity2:forall (x y:CReal),
-  (((C2D x)*(C2D y))%DR==C2D (x*y)%CR)%DR.
-Proof.
-Admitted.
-
-Theorem C2D_properity3:forall (x y:CReal),
-(x==y)%CR->  ((C2D x)==(C2D y))%DR.
-Proof.
-Admitted.
-
-
-(* Fourth Section: D2C *)
-Fixpoint psucc(n:positive):positive :=
-match n with
-| xI p => xO (psucc p)
-| xO p => xI p
-| xH => xO xH
-end.
-
-Fixpoint of_nat(n:nat) : positive :=
- match n with
-   | O => 1
-   | S O => 1
-   | S x => psucc (of_nat x)
- end.
-Theorem Cauchy_Dcut :forall (DCut:Q->Prop),
-Dedekind DCut->Cauchy (fun n q=>exists N:Z,
-DCut (N#(2^(of_nat n)))/\~DCut((N+1)%Z#2^(of_nat n))/\(q==N#2^(of_nat n))%Q).
-Proof.
-Admitted.
-
-Definition D2C (B:DReal):CReal :=
-match B with
-|Real_cons DCut H =>
-Real_intro (fun n q=>exists N:Z,DCut (N#(2^(of_nat n)))/\~DCut((N+1)%Z#2^(of_nat n))/\(q==N#2^(of_nat n))%Q)
-(Cauchy_Dcut DCut H) end.
-
-(* Fifth Section: Bijection *)
-Theorem Bijection: forall x y,(D2C x==y)%CR <->(C2D y==x)%DR.
+Theorem Bijection: forall x y,(D2C x==y) <->(C2D y==x).
 Proof.
 Admitted.
---
+++ Iso/Dedekind2Cauchy.v
@@ -0,0 +1,54 @@
+Set Warnings "-notation-overridden,-parsing".
+From Coq Require Import Bool.Bool.
+From Coq Require Import Logic.Classical.
+From Coq Require Import Init.Nat.
+From Coq Require Import Arith.Arith.
+From Coq Require Import Arith.EqNat.
+From Coq Require Import omega.Omega.
+From Coq Require Import Lists.List.
+From Coq Require Import Strings.String.
+From Coq Require Import QArith.QArith_base.
+From Coq Require Import QArith.Qabs.
+From Coq Require Import QArith.Qminmax.
+From Coq Require Import Logic.Classical.
+From Coq Require Import Classes.Equivalence.
+From Coq Require Import Classes.Morphisms.
+From CReal Require Import Dedekind.RBase.
+From CReal Require Import Dedekind.ROrder.
+From CReal Require Import Dedekind.RArith.
+Require Import CReal.Cauchy.RBase_uncomp.
+From Coq Require Import PArith.BinPosDef.
+Import Pos.
+
+Module C := Cauchy.RBase_uncomp.
+Module D1 := Dedekind.RBase.
+Module D2 := Dedekind.ROrder.
+Module D3 := Dedekind.RArith.
+
+Theorem Cauchy_Dcut :forall (DCut:Q->Prop),
+Dedekind DCut->Cauchy (fun n q=>exists N:Z,
+DCut (N#(2^(of_nat n)))/\~DCut((N+1)%Z#2^(of_nat n))/\(q==N#2^(of_nat n))%Q).
+Proof.
+Admitted.
+
+Definition D2C (B:D1.Real):C.Real :=
+match B with
+|Real_cons DCut H =>
+Real_intro (fun n q=>exists N:Z,DCut (N#(2^(of_nat n)))/\~DCut((N+1)%Z#2^(of_nat n))/\(q==N#2^(of_nat n))%Q)
+(Cauchy_Dcut DCut H) end.
+
+Theorem C2D_properity1:forall (x y:D1.Real),
+ C.Real_equiv(C.Rplus (D2C x)(D2C y))(D2C (D3.Rplus x y)).
+Proof.
+Admitted.
+
+Theorem C2D_properity2:forall (x y:D1.Real),
+ C.Real_equiv (C.Rmult(D2C x)(D2C y))(D2C (D3.Rmult x y)).
+Proof.
+Admitted.
+
+Theorem C2D_properity3:forall (x y:D1.Real),
+(x==y)->  (C.Real_equiv (D2C x)(D2C y)).
+Proof.
+Admitted.
+
