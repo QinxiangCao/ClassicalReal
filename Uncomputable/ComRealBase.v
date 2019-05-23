432b3563937333c83efa0a66e975c8243a57c4e6
WxWyashen <21474008@qq.com>
2019-05-23 23:41:47 +0800

    update in 2019.5.23

--- Uncomputable/ComRealBase.v
+++ Uncomputable/ComRealBase.v
@@ -1,4 +1,5 @@
-(** Uncomputable in the definition of R function **)
+(** Uncomputablity in the definition of R function **)
+(** For convenience's sake, we focus on real numbers in [0,1] **) 
 Set Warnings "-notation-overridden,-parsing".
 From Coq Require Import Bool.Bool.
 From Coq Require Import Logic.Classical.
@@ -83,50 +84,64 @@ Module Type Vir_R.
     intros.
     unfold Proper.
     apply Reqb_refl.
-  Qed.  
+  Qed.
+
+  Axiom Up_same : forall (r1 r2 : R) , r1 = r2 -> (up r1 = up r2)%Z.
+  Axiom Up_R0 : (up R0 = 0)%Z.
+  Axiom Rlt_trans : forall r1 r2 r3:R, r1 < r2 -> r2 < r3 -> r1 < r3.
+  Axiom Rlt_not_refl : forall r1 : R , ~ (r1 < r1).
+  Theorem Req : forall (x y : R) , x <= y /\ y <= x <-> x = y.
+  Proof.
+    intros.
+    split.
+    - intros.
+      destruct H ; auto.
+      destruct H ; auto.
+      destruct H0 ; auto.
+      pose proof Rlt_trans x y x H H0.
+      exfalso. apply (Rlt_not_refl x) ; auto.
+    - intros.
+      split ; unfold Rle in * ; unfold Rge in *; auto.
+  Qed.
+  
+  Theorem Rle_ge : forall (x y : R) , x >= y <-> y <= x.
+  Proof.
+    intros.
+    unfold Rge in *.
+    unfold Rle in *.
+    split ; intros ; destruct H ; auto.
+  Qed.
+  
+  Parameter Rplus_0 : forall r1 r2 : R , r1 - r2 = R0 <-> r1 = r2.
+  Parameter IZR_0 : forall z : Z , IZR z = R0 <-> (z = 0)%Z.
+  Theorem IZR_0' : IZR 0 = R0.
+  Proof.
+    apply IZR_0 ; auto.
+  Qed.
+  Parameter Z_same_R : forall z1 z2 : Z , (z1 = z2)%Z <-> IZR z1 = IZR z2.
+  Theorem Z_same_R' : forall z1 z2 : Z , (z1 <> z2)%Z <-> IZR z1 <> IZR z2.
+  Proof.
+    unfold not in *.
+    intros. 
+    split ; intros ; apply H ; apply Z_same_R ; auto.
+  Qed.
+  Theorem Ex_Z_R' : forall z : Z , (z <> 0)%Z <-> IZR z <> R0.
+  Proof.
+    unfold not in *.
+    intros.
+    split ; intros ; apply H ; apply IZR_0 ; auto.
+  Qed.
   
   Axiom Rle_ge_eq : forall x y : R , x >= y -> y >= x -> x = y.
-
-  Record Function_prop (f : R -> R -> Prop) : Prop := {
-    propertise1 : forall x x1 y y1 : R ,  f x y -> f x1 y1 -> x = x1 ->y = y1;
-    propertise2 : forall x :R , exists y : R , f x y;
-    propertise3 : Proper(Reqb ==> Reqb ==> iff) f;
-  }.
-
-  Record Single_function_prop(f : R -> Prop) : Prop := {
-    Propertise1 : forall x x1: R ,  f x -> f x1 -> x = x1;
-    Propertise2 : exists x : R , f x;
-    Propertise3 : Proper(Reqb ==> iff) f;
-  }.
-
-  Definition G_signle_exists : {f : R -> Prop | Single_function_prop f} -> R.
-    Admitted.
-
-  Definition G_function_exists : {f : R -> R -> Prop | Function_prop f} -> (R -> R).
-    (*
-    intros.
-    destruct X.
-    destruct f.
-    assert (H' : Single_function_prop (x X0)).
-    { split ; auto. 
-      - intros. apply (propertise4 X0 X0 x0 x1) ; auto.
-      - split.
-        + assert (H'' : Reqb X0 X0).
-          { reflexivity. }
-          destruct (propertise6 X0 X0 H'' x0 y) ; auto.
-        + assert (H'' : Reqb X0 X0).
-          { reflexivity. }
-          destruct (propertise6 X0 X0 H'' x0 y) ; auto.
-    }
-    pose proof (G_single_exists x X0 H').
-    auto.
-  Defined.
-    *)
-    Admitted.
 
   (** The definition of R Number matched with Turing machine *)
   Parameter Bin_R : R -> nat -> nat -> Prop.
   Parameter TMR : nat -> R -> Prop.
+
+  Axiom TMR_pro0 : forall (r : R) (n : nat) , TMR n r -> r >= R0.
+  Axiom TMR_pro0' : forall (r : R) (n : nat) , TMR n r -> r <= R1.
+  (** These two axioms above may be proved by other axioms. *)
+
   Axiom Bin_R_pro1 : forall (r : R) (n : nat) , Bin_R r n 1 \/ Bin_R r n 0.
   Axiom Bin_R_pro2 : forall (r : R) (n : nat) , Bin_R r n 1 <-> (~ Bin_R r n 0).
   Axiom Zero_Bin : forall (n : nat) , Bin_R R0 n 0.
@@ -173,7 +188,7 @@ Module Type Vir_R.
       rewrite (H4 _ H1). apply iff_refl.
   Qed.
 
-  Theorem Bin_R_pro3_not : forall (r1 r2 : R) , r1 <> r2 <-> exists (j : nat), ~(Bin_R r1 j 1 <-> Bin_R r2 j 1).
+  Theorem Bin_R_pro3_not : forall (r1 r2 : R) , r1 <> r2 <-> exists (j : nat), (Bin_R r1 j 1 <-> Bin_R r2 j 0).
   Proof.
     intros.
     split.
@@ -182,12 +197,26 @@ Module Type Vir_R.
       rewrite H0 in H.
       clear H0.
       apply not_all_ex_not in H.
-      apply H.
+      destruct H.
+      exists x.
+      unfold not in H.
+      split.
+      + intros. apply Bin_R_pro2'. unfold not. intros. apply H.
+        split ; auto.
+      + intros. apply Bin_R_pro2. unfold not. intros. apply H.
+        split ; intros ; apply Bin_R_pro2 in H2 ; exfalso ; apply H2 ; auto.
     - intros.
       destruct H.
       unfold not in *.
-      intros. apply H.
-      apply (Bin_R_pro3 r1 r2) ; auto.
+      intros.
+      rewrite (Bin_R_pro3 r1 r2) in H0.
+      rewrite (H0 x) in H.
+      rewrite (Bin_R_pro2 r2 x) in H.
+      destruct H.
+      pose proof classic (Bin_R r2 x 0).
+      destruct H2.
+      + apply H1 ; auto.
+      + apply H2 ; auto.
   Qed.
   
   Axiom R_is_TMR : forall (n:nat) (r:R) , TMR n r <-> (forall (j : nat), TM n j -> Bin_R r j 1) /\ (forall (j:nat) , ~ TM n j -> Bin_R r j 0).
@@ -270,21 +299,7 @@ Module Type Vir_R.
       apply (only_R_TMR n1 r2 r1) ; auto.
       apply (R_only_TMR n2 n1 r2) ; auto.
   Qed.
-
-  Theorem TMR_obey_Single_function_prop : forall (n : nat) , Single_function_prop (TMR n).
-  Proof.
-    intros.
-    split.
-    - apply only_R_TMR.
-    - exists (Get_TMR n). apply R_ex_TMR.
-    - split ; intros. 
-      + apply (only_R_TMR n x y) ; auto.
-      + apply (only_R_TMR n y x) ; auto.
-  Qed.
-
-
-  Parameter CR : R -> Prop.
-    
+   
   Theorem P_OR_NP : (forall P : Prop, {P} + {~ P}) -> Halting.
   Proof. 
     unfold Halting.
@@ -323,23 +338,11 @@ Module Type Vir_R.
       apply (TMR_proper0 i X0 H x).
       apply Bin_R_pro2.
       unfold not in *.
-      intros. apply H1.
-      split ; intros .
-      + exfalso. apply Bin_R_pro2 in H3. apply H3 ; auto.
-      + exfalso. apply Bin_R_pro2 in H3. apply H3. apply Zero_Bin.
+      intros. 
+      apply Bin_R_pro2' in H2.
+      apply H2. apply H1.
+      apply Zero_Bin.
   Qed.
-
-  Theorem Up_R : (forall (r : R) (z : Z) , IZR z <= r /\ r <= IZR (z + 1) -> up r = z) -> Halting.
-  Proof.
-    unfold Halting.
-    intros.
-    pose proof R_ex_TMR i.
-    remember (Get_TMR i) as X0.
-    pose proof R_ex_TMR 0.
-    rewrite Zero_TMR in *.
-    clear HeqX0.
-  Admitted.
-  (** This theorem also proves the uncomputability of Int_part frac_part *)
 
   Theorem Three_dimensions : (forall r1 r2 : R, {r1 = r2} + {r1 > r2} + {r1 < r2}) -> Halting.
   Proof. 
@@ -357,8 +360,7 @@ Module Type Vir_R.
       exists x.
       unfold not.
       intros.
-      apply Bin_R_pro2' in H2.
-      apply H2. apply H ; auto.
+      split ; auto.
     - right.
       apply Bin_R_pro3_not.
       apply Bin_R_pro3' in r.
@@ -366,8 +368,9 @@ Module Type Vir_R.
       exists x.
       unfold not.
       intros.
-      apply Bin_R_pro2' in H1.
-      apply H1. apply H ; auto.
+      split ; intros ; exfalso.
+      + apply Bin_R_pro2 in H. apply H ; auto.
+      + apply Bin_R_pro2' in H. apply H ; auto.
   Qed.
   
   Theorem Two_dimensions2 : (forall r1 r2 : R, {r1 >= r2} + {r1 < r2}) -> Halting.
@@ -383,19 +386,143 @@ Module Type Vir_R.
     apply Rle_ge_eq ; auto.
   Qed.
   
+  Theorem Exam_dimensions2 : (forall r : R , {r = R0} + {r <> R0}) -> Halting.
+  Proof.
+    unfold Halting.
+    intros.
+    apply Two_dimensions.
+    intros.
+    pose proof X (r1 - r2).
+    destruct H.
+    - left. apply Rplus_0 ; auto.
+    - right. unfold not in *.  intros. apply n. apply Rplus_0 ; auto.
+  Qed.
   (** This theorem also proves the uncomputability of Rmax Rmin Rabs Rdist*)
   (** Rmax Rmin Rabs Rdist are actually computable. Defined in an incorrect way in std. -- Qinxiang *)
 
-  Theorem Rinv_def : forall (Rinv' : R -> R)(r: R) (H : r <> R0) ,Rinv' r = Rinv (exist _ r H) -> Halting.
-  Proof.  intros. Admitted.
+  Parameter RnZ : R -> nat -> Z. (** use the first n numbers of R to construct a member of Z *)
+  Axiom RnZ_pro1 : forall (r : R) (n : nat) , (forall (j : nat) , (j > n)%nat -> (Bin_R (IZR (RnZ r n)) j 0)). 
+  Axiom RnZ_pro2 : forall (r : R) (n : nat) , (forall (j : nat) , (j <= n)%nat -> (Bin_R r j 1 <-> Bin_R (IZR (RnZ r n))j 1)).
+
+  Lemma Turing_eqb_1 : forall (n : nat) (r : R), TMR n r -> ((forall (j : nat) , (RnZ r j = 0)%Z) <-> (forall (j : nat), ~ TM n j)).
+  Proof.
+    intros.
+    pose proof TMR_proper0 n r H.
+    split ; intros.
+    - pose proof H1 j.
+      apply (IZR_0 (RnZ r j)) in H2.
+      apply H0. 
+      apply Bin_R_pro2'. unfold not in *.
+      intros.
+      apply (RnZ_pro2 r j j) in H3 ; auto.
+      rewrite H2 in H3.
+      pose proof Zero_Bin j.
+      apply Bin_R_pro2' in H4.
+      apply H4 ; auto.
+    - apply Z_same_R. rewrite IZR_0'.
+      apply Bin_R_pro3. intros.
+      pose proof le_gt_dec j0 j.
+      destruct H2. 
+      + pose proof RnZ_pro2 r j j0 l.
+        rewrite <- H2.
+        split.
+        * intros. apply H0 in H3.
+          apply Bin_R_pro2. unfold not in *.
+          intros. apply (H1 j0) ; auto.
+        * intros. exfalso. apply Bin_R_pro2 in H3.
+          apply H3. apply Zero_Bin.
+      + pose proof RnZ_pro1 r j j0 g.
+        split.
+        * intros. exfalso. apply Bin_R_pro2 in H3. apply H3;auto.
+        * intros. exfalso. apply Bin_R_pro2 in H3. apply H3. apply Zero_Bin.
+  Qed.
+
+  Lemma Turing_eqb_1' : forall (n : nat) (r : R), TMR n r -> ((exists (j : nat) , (RnZ r j <> 0)%Z) <-> (exists (j : nat), TM n j)).
+  Proof.
+    intros.
+    pose proof TMR_proper0 n r H.
+    split ; intros ; destruct H1.
+    - apply Ex_Z_R' in H1.
+      apply Bin_R_pro3_not in H1.
+      destruct H1. exists x0.
+      apply H0.
+      pose proof le_gt_dec x0 x.
+      destruct H2. 
+      + pose proof RnZ_pro2 r x x0 l.
+        apply H2. apply H1. apply Zero_Bin.
+      + pose proof RnZ_pro1 r x x0 g.
+        apply Bin_R_pro2' in H2.
+        exfalso. apply H2. apply H1. apply Zero_Bin.
+    - exists x.
+      apply Ex_Z_R'. 
+      apply Bin_R_pro3_not.
+      exists x.
+      assert (H' : (x <= x) %nat). { auto. }
+      pose proof RnZ_pro2 r x x H'. clear H'.
+      rewrite <- H2.
+      split ; intros.
+      + apply Zero_Bin.
+      + apply (R_is_TMR n r) in H.
+        apply H in H1 ; auto.
+  Qed.
+  
+  Theorem Up_R : (forall (r : R) (z : Z) , IZR (z - 1) < r /\ r <= IZR z <-> (up r = z)%Z) -> Halting.
+  Proof.
+    unfold Halting.
+    intros.
+    pose proof R_ex_TMR i.
+    remember (Get_TMR i) as X0.
+    clear HeqX0.
+    pose proof Z.eq_dec (up X0) 0%Z.
+    pose proof (TMR_pro0 X0 i H0).
+    destruct H1.
+    - rewrite <- (H X0 0%Z) in e.
+      simpl in *. destruct e.
+      rewrite IZR_0' in H3.
+      apply (Rle_ge X0 R0) in H2.
+      pose proof conj H3 H2.
+      apply (Req X0 R0) in H4.
+      right.
+      rewrite (Bin_R_pro3 X0 R0) in H4.
+      intros.
+      clear H3 H2 H1.
+      apply (TMR_proper0 i X0 H0 j).
+      apply Bin_R_pro2'. unfold not.
+      intros. apply H4 in H1.
+      apply Bin_R_pro2 in H1.
+      apply H1. apply Zero_Bin.
+    - left. destruct H2.
+      + apply Bin_R_pro3' in H1.
+        destruct H1. exists x.
+        apply (TMR_proper0 i X0 H0 x).
+        destruct H1 as [ ? [?  ? ]] ; auto.
+      + apply Up_same in H1.
+        exfalso. apply n.
+        rewrite H1. apply Up_R0.
+  Qed.
+  (** This theorem also proves the uncomputability of Int_part frac_part *)
+
+  Theorem Rinv_def : (forall (Rinv' : R -> R)(r: R) (H : r <> R0) ,Rinv' r = Rinv (exist _ r H)) -> Halting.
+  Proof.  
+    intros. 
+    apply Exam_dimensions2.
+    intros.
+    
+  Admitted.
   Theorem Rpow_def : forall (Rpow' : R -> nat -> R)(r: R)(H : r <> R0) ,Rpow' r 0%nat = Rpow (exist _ r H) 0%nat -> Halting.
-  Proof.  intros. Admitted.
+  Proof.  
+    intros. 
+  Admitted.
   (** This theorem also proves the problem in the definition of powRZ *)
 
+  Parameter CR : R -> Prop.
+  Axiom TMR_is_CR : forall (r : R) (n : nat) , TMR n r -> CR r.
   Parameter Un_cv : (nat -> R) -> R -> Prop. (** the limitation of a list of Real Number *)
   
-  Theorem lim_CN_NCN :forall (Un:nat -> R) (l1:R), Un_cv Un l1 -> (forall n : nat ,CR (Un n)) -> ~ CR l1.
-  Proof. Admitted.
+  Theorem lim_CN_NCN : (forall (Un:nat -> R) (l1:R), Un_cv Un l1 -> (forall n : nat ,CR (Un n)) -> CR l1) -> Halting.
+  Proof. 
+    
+  Admitted.
   
 End Vir_R.
 
