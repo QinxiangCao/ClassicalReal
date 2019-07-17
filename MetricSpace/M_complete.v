0f7d6a2636e0dcc390ade3714ece6f5af3e121b8
shrubbroom <shrubbroom@sjtu.edu.cn>
2019-07-17 21:24:44 +0800

    common push

---
+++ MetricSpace/M_complete.v
@@ -0,0 +1,4 @@
+From CReal.MetricSpace Require Export M_pack.
+From CReal.MetricSpace Require Export M_new.
+(**This part will define what is a complete space and show that the space with Type
+      Cauchilize eqX eqA leX plusX is complete**)
