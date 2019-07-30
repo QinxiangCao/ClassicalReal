fef5e20ed4f751a235e0bdedbe9096f3de7529f9
WxWyashen <21474008@qq.com>
2019-07-30 18:15:40 +0800

    finish the definition of TM12

--- Uncomputable/TMSet.v
+++ Uncomputable/TMSet.v
@@ -20,41 +20,86 @@ Import ListNotations.
 
 
 Module TM_0_0.
-  Parameter TM : nat -> nat -> nat.
+  Parameter TM : nat -> nat -> bool.
+   (** change nat into bool *)
   (** The i-th Turing machine can stop in m steps or not . **)
-Definition Halting : Type := forall i : nat , {exists j, TM i j = 1%nat} + {forall j , TM i j = 0%nat}.
-Definition Halting_easy : Type := exists f : Halting , True.
-Definition Halting_easy' : Type := forall i : nat , exists f : ({exists j, TM i j = 1%nat} + {forall j , TM i j = 0%nat}), True.
-(** forall n,exists b : {P n} +{~ P n} , True *)
+  Definition Halting : Type := forall i : nat , {exists j, TM i j = true} + {forall j , TM i j = false}.
+  Definition Halting_easy : Type := exists f : Halting , True.
+  Definition Halting_easy' : Type := forall i : nat , exists f : ({exists j, TM i j = true} + {forall j , TM i j = false}), True.
+  (** forall n,exists b : {P n} +{~ P n} , True *)
 
-Theorem Halting_arrow : Halting -> Halting_easy.
-Proof.
-  unfold Halting_easy.
-  unfold Halting.
-  intros.
-  exists H ; auto.
-Qed.
+  Theorem Halting_arrow : Halting -> Halting_easy.
+  Proof.
+    unfold Halting_easy.
+    unfold Halting.
+    intros.
+    exists H ; auto.
+  Qed.
 
-Theorem Halting_arrow' : Halting_easy -> Halting_easy'.
-Proof.
-  unfold Halting_easy.
-  unfold Halting_easy'.
-  unfold Halting.
-  intros.
-  destruct H.
-  exists (x i) ; auto.
-Qed.
+  Theorem Halting_arrow' : Halting_easy -> Halting_easy'.
+  Proof.
+    unfold Halting_easy.
+    unfold Halting_easy'.
+    unfold Halting.
+    intros.
+    destruct H.
+    exists (x i) ; auto.
+  Qed.
 
-Axiom Turing_proper0 : forall (i j : nat) , TM i j = 0%nat \/ TM i j = 1%nat.
-Axiom Turing_proper1 : forall (i j k: nat), (j <= k)%nat -> TM i j = 1%nat -> TM i k = 1%nat.
-Theorem Turing_proper2 : forall (i j k: nat), (k <= j)%nat -> TM i j = 0%nat -> TM i k = 0%nat.
-Proof.
-  unfold not in *.
-  intros.
-  destruct (Turing_proper0 i k) ; auto.
-  apply (Turing_proper1 i k j) in H1 ; auto.
-  rewrite H0 in H1.
-  discriminate H1.
-Qed.
+  Axiom Turing_proper1 : forall (i j k: nat), (j <= k)%nat -> TM i j = true -> TM i k = true.
+  Theorem Turing_proper2 : forall (i j k: nat), (k <= j)%nat -> TM i j = false -> TM i k = false.
+  Proof.
+    unfold not in *.
+    intros.
+    destruct (TM i k) eqn : H1 ; auto.
+    apply (Turing_proper1 i k j) in H1 ; auto.
+    rewrite H0 in H1.
+    discriminate H1.
+  Qed.
 
 End TM_0_0.
+
+Module TM_1_2.
+  Parameter TM12 : Type.
+  Parameter eval_1_2 : TM12 -> nat -> nat -> option(nat * nat).
+  Axiom TM_pro0 : forall (f : nat -> (nat * nat)) , exists (i : TM12), forall (x : nat) , exists (j : nat),
+                  eval_1_2 i x j = Some (f x).
+  Axiom TM_pro1 : forall (i : TM12) (x j1 j2: nat)(z : nat * nat), (j1 >= j2)%nat -> eval_1_2 i x j2 = Some z -> eval_1_2 i x j1 = Some z.
+  Theorem TM_pro1' : forall (i : TM12) (x j1 j2 : nat) , (j1 <= j2)%nat -> eval_1_2 i x j2 = None -> eval_1_2 i x j1 = None.
+  Proof.
+    intros.
+    destruct (eval_1_2 i x j1) eqn:H1 ; auto.
+    pose proof TM_pro1 i x j2 j1 p H H1.
+    rewrite H0 in H2.
+    discriminate H2.
+  Qed.
+  
+  Definition Halting : Type := 
+          forall (i : TM12)(x : nat), 
+            {exists (j : nat) (z : nat * nat),eval_1_2 i x j = Some z}
+          + {forall (j : nat) , eval_1_2 i x j = None}.
+  Definition Halting_easy : Type := exists f : Halting , True.
+  Definition Halting_easy' : Type := forall (i:TM12)(x:nat) ,exists f : 
+        ({exists (j:nat)(z : nat * nat), eval_1_2 i x j = Some z} + 
+        {forall (j:nat) , eval_1_2 i x j = None}), True.
+  (** forall n,exists b : {P n} +{~ P n} , True *)
+
+  Theorem Halting_arrow : Halting -> Halting_easy.
+  Proof.
+    unfold Halting_easy.
+    unfold Halting.
+    intros.
+    exists X ; auto.
+  Qed.
+
+  Theorem Halting_arrow' : Halting_easy -> Halting_easy'.
+  Proof.
+    unfold Halting_easy.
+    unfold Halting_easy'.
+    unfold Halting.
+    intros.
+    destruct H.
+    exists (x0 i x) ; auto.
+  Qed.
+  
+End TM_1_2.
